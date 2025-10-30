d8335c17c0f39eafee110bc7130717562857c67a
Marcus ganz <marcusganz737@gmail.com>
2025-10-29 23:56:05 -0700

    starter Code

---
+++ TrafficAnalysis.py
@@ -0,0 +1,13 @@
+#Marcus Ganz V00952336
+import sys
+import ssl
+import socket
+import urllib.parse 
+
+print("the file path that was typed was:", sys.argv[1:])
+argument = sys.argv[1:]
+tcpFile = argument[0]
+
+print(tcpFile)
+
+
