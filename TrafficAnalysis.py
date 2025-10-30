313ebbfe79952b56b8005a5b3dd69dd0121e69b3
Marcus ganz <marcusganz737@gmail.com>
2025-10-30 14:29:10 -0700

    opening file

--- TrafficAnalysis.py
+++ TrafficAnalysis.py
@@ -3,11 +3,24 @@ import sys
 import ssl
 import socket
 import urllib.parse 
+import struct
+
+def readFile(filePath):
+    with open(filePath, 'rb') as f:
+        x = f.readlines()
+        # this is just here to test 
+#        for y in x:
+#            print(y)
+        return x
+
 
 print("the file path that was typed was:", sys.argv[1:])
 argument = sys.argv[1:]
 tcpFile = argument[0]
 
 print(tcpFile)
+capAsList = readFile(tcpFile)
+
+
 
 
